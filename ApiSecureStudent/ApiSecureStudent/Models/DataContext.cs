ddab852b128c619114d7c378fe520e1b7c006f06
marialaurasc311 <marialaurasc311@gmail.com>
2020-08-29 10:13:04 -0400

    Con Controlador Students+Authorize

---
+++ ApiSecureStudent/ApiSecureStudent/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiSecureStudent.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiSecureStudent.Models.Student> Students { get; set; }
+    }
+}
---
+++ ApiSecureStudent/ApiSecureStudent/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace ApiSecureStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Range(1, 9999)]
+        public int StudentId { get; set; }
+
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+
+
+        [StringLength(30, ErrorMessage = "The field {0} must contain between {2} and {1} characters", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
